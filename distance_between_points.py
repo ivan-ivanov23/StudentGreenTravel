1a6c01650309b4c5cbf1e192e00579331efadeff
ivan-ivanov23 <83659450+ivan-ivanov23@users.noreply.github.com>
2024-01-22 16:34:51 +0000

    code cleaning

--- distance_between_points.py
+++ distance_between_points.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import geopy.distance
+import airportsdata
 
 
 # Open addresses file and for each postcode, get the longitude and latitude from ukpostcodes.csv
@@ -27,25 +28,11 @@ for i in df['Home_Postcode']:
             result[i] = [df_postcodes.loc[df_postcodes['postcode'] == i, 'latitude'].iloc[0], df_postcodes.loc[df_postcodes['postcode'] == i, 'longitude'].iloc[0]]
 
 # print(result)
-
-# Get the longitude and latitude of uni of aberdeen AB24 3FX from ukpostcodes.csv
-uni_lat = df_postcodes.loc[df_postcodes['postcode'] == 'AB24 3FX', 'latitude'].iloc[0]
-# print("Uni of Aberdeen latitude: ", uni_lat)
-uni_long = df_postcodes.loc[df_postcodes['postcode'] == 'AB24 3FX', 'longitude'].iloc[0]
-# print("Uni of Aberdeen longitude: ", uni_long)
-
-coords_1 = (uni_lat, uni_long)
+            
 # coords_2 is the longitude and latitude of the first postcode in the results dict which is HA9 0WS
-# coords_2 = (result['HA9 0WS'][0], result['HA9 0WS'][1])
-# Try doing it without hardcoding the postcode
-coords_2 = (result[df['Home_Postcode'][0]][0], result[df['Home_Postcode'][0]][1])
+coords_2 = (result[df['Home_Postcode'][2]][0], result[df['Home_Postcode'][2]][1])
 
 
-# https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
-# print(round(geopy.distance.geodesic(coords_1, coords_2).km,2), "km")
-
-import airportsdata
-import pgeocode
 airports = airportsdata.load('IATA')  # key is the IATA location code
 # Add all UK airports to a dict with their city as the key and longitude and latitude as the value
 airports_dict = {}
@@ -58,11 +45,12 @@ for i in airports:
 # Create a new dict to store the distances with the airport as the key and the distance as the value
 distances = {}
 for i in airports_dict:
+    # https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
     distances[i] = round(geopy.distance.geodesic(coords_2, (airports_dict[i][0], airports_dict[i][1])).km,2)
 
 # Select the airport with the smallest distance and print the distance and the airport
 # print the postcode that we are using
-print('Closest airport to', df['Home_Postcode'][0], 'is: ')
+print('Closest airport to', df['Home_Postcode'][2], 'is: ')
 print(min(distances, key=distances.get), min(distances.values()), "km")
 
 
