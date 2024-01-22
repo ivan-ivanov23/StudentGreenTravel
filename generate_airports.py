005c3dadc74446edc153a1de35fc779c5a3dbf95
ivan-ivanov23 <83659450+ivan-ivanov23@users.noreply.github.com>
2024-01-22 17:05:16 +0000

    generate GBairports

---
+++ generate_airports.py
@@ -0,0 +1,18 @@
+# This file was used to generate the GBairports.csv file which is used in distance_between_points.py
+# The GBairports.csv file contains only the public UK airports and their longitude and latitude
+# This filtering was done manually in Excel with the help of wikipedia
+
+import airportsdata
+import pandas as pd
+
+airports = airportsdata.load('IATA')  # key is the IATA location code
+# Add all UK airports to a dict with their city as the key and longitude and latitude as the value
+airports_dict = {}
+for i in airports:
+    if airports[i]['country'] == 'GB':
+        airports_dict[airports[i]['name']] = [airports[i]['lat'], airports[i]['lon']]
+# print(airports_dict)
+        
+# Save airports_dict to a csv file
+df_airports = pd.DataFrame.from_dict(airports_dict, orient='index', columns=['Latitude', 'Longitude'])
+df_airports.to_csv('GBairports.csv')
