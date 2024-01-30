60e0fb83db7693a3050aa7dbdc729fdf4efb4513
ivan-ivanov23 <83659450+ivan-ivanov23@users.noreply.github.com>
2024-01-30 11:13:49 +0000

    add comments

--- travel_distance.py
+++ travel_distance.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from geopy.distance import geodesic
-import tkinter
 from tkinter.filedialog import askopenfile
 
 
@@ -9,6 +8,7 @@ from tkinter.filedialog import askopenfile
 aberdeen_airport = (57.2019004822, -2.1977798939)
 aberdeen_uni = (57.1645, -2.0999)
 
+"""=========================================File Explorer window============================================================"""
 # open file explorer for excel files only so that it is utf-8 encoded
 file = askopenfile(filetypes=[("Excel files", "*.xlsx")])
 # If the user selected a file, then read it using pandas
@@ -23,7 +23,7 @@ if file:
 else:
     exit()
 
-
+"""=========================================Read Data============================================================"""
 # Read ukpostcodes.csv
 postcodes = pd.read_csv('data/ukpostcodes.csv')
 # Drop index column
@@ -35,12 +35,11 @@ postcode_array = postcodes['postcode'].values
 latitude_array = postcodes['latitude'].values
 longitude_array = postcodes['longitude'].values
 
-# Create a dictionary to store postcode coordinates
+# Create a dictionary to store postcode coordinates 
 postcode_coord_dict = dict(zip(postcode_array, zip(latitude_array, longitude_array)))
 
 # Read in the airports csv file
 airports = pd.read_csv("data/GBairports.csv")
-
 # Convert DataFrame columns to numpy arrays for faster processing
 airport_name_array = airports['Unnamed: 0'].values
 airport_latitude_array = airports['Latitude'].values
@@ -49,22 +48,28 @@ airport_longitude_array = airports['Longitude'].values
 # Create a dictionary to store airport coordinates
 airport_coord_dict = dict(zip(airport_name_array, zip(airport_latitude_array, airport_longitude_array)))
 
-
+"""=========================================Main Functions============================================================"""
 def calculate_distances(coords1, coords2_array):
     """Calculate distances between two coordinates and an array of coordinates."""
     distances = []
+    # For single postcode coordinates in array of coordinates
     for coords2 in coords2_array:
+        # If any of the two are nan
         if np.isnan(coords2).any() or np.isnan(coords1).any():
             distances.append(np.nan)
         else:
+            # Find distance in km
             distances.append(geodesic(coords1, coords2).km)
     return np.array(distances)
 
 def closest_airport(postcode, postcode_coords, airports_dict):
     """Returns the closest airport to the postcode"""
     if postcode in postcode_coords:
+        # calculate distances in km
         distances = calculate_distances(postcode_coords[postcode], np.array(list(airports_dict.values())))
+        # Find the airport with shortest distance to postcode
         closest_idx = np.argmin(distances)
+        # Return the airport name and distance of the closest airport
         return list(airports_dict.keys())[closest_idx], distances[closest_idx]
     else:
         # If the postcode is invalid, return Aberdeen airport as the closest with distance 0
@@ -72,16 +77,19 @@ def closest_airport(postcode, postcode_coords, airports_dict):
 
 def travel(postcode_coords, airports_dict):
     """Returns a dictionary with postcodes as keys and closest airports as values"""
+    # For postcode in column 2 of address file
     for postcode in addresses.iloc[:, 1]:
+        # Find the closest airport to the postcode
         closest_airport_name, distance = closest_airport(postcode, postcode_coords, airports_dict)
         print(f'Closest airport to {postcode} is {closest_airport_name} ~ {distance:.2f} km')
         
+        # If the closest airport is not Aberdeen (default value for nan postcodes)
         if closest_airport_name != 'Aberdeen':
             # Calculate the distance between the two airports
             travel_distance = geodesic(airports_dict[closest_airport_name], aberdeen_airport).km
             print(f'The distance between {closest_airport_name} and Aberdeen airport is {travel_distance:.2f} km')
         else:
-            # For Aberdeen airport, set the distance to 0
+            # For default value, set the distance to 0
             travel_distance = 0
             print(f'The distance between {closest_airport_name} and Aberdeen airport is {travel_distance:.2f} km')
 
@@ -91,5 +99,6 @@ def travel(postcode_coords, airports_dict):
     uni_airport_distance = geodesic(aberdeen_airport, aberdeen_uni).km
     print(f'The distance between Aberdeen airport and the university is {uni_airport_distance:.2f} km')
 
-# Execute
+
+"""=========================================Execute============================================================"""
 travel(postcode_coord_dict, airport_coord_dict)
