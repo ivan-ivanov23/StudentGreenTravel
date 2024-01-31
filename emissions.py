a78d7f6cf3ca06d8744d03a481797a3760cc664c
ivan-ivanov23 <83659450+ivan-ivanov23@users.noreply.github.com>
2024-01-31 17:38:07 +0000

    add TODO in emissions.py

--- emissions.py
+++ emissions.py
@@ -2,3 +2,9 @@ from travel_distance import info
 
 
 print(info)
+
+
+# TODO
+# If postcode is in Scotland, it should consider not flying
+# Make it calculate emissions based on road travel distance and air travel distance
+
