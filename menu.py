e2b526ebbb74a92306f728d2634ce48e7a2014aa
ivan-ivanov23 <83659450+ivan-ivanov23@users.noreply.github.com>
2024-02-18 11:16:24 +0000

    add comoboxes in page1

--- menu.py
+++ menu.py
@@ -1,8 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QTableView 
-from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QLabel, QLineEdit
-from PyQt6.QtCore import Qt, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QGridLayout, QComboBox
+from PyQt6.QtCore import Qt, pyqtSignal, QSize
 from main import main
 from tkinter.filedialog import askopenfile
 import pandas as pd
@@ -16,15 +14,13 @@ class WelcomePage(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setStyleSheet("background-color: rgb(193, 225, 193);")
-
         self.title_label = QLabel("Welcome to StudentGreenTravel", self)
         self.title_label.setStyleSheet("font-size: 34px; font-weight: bold; color: #2d3436;")
         self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         # Buttons for menu
         button1 = QPushButton("Emission Calculator", clicked=lambda: self.show_page(Page1()))
-        button1.setEnabled(False)
+        #button1.setEnabled(False)
         button2 = QPushButton("Display Routes", clicked=lambda: self.show_page(Page2()))
         button2.setEnabled(False)
         button3 = QPushButton("Statistics", clicked=lambda: self.show_page(Page3()))
@@ -50,14 +46,12 @@ class WelcomePage(QWidget):
         self.file_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.main_layout = QVBoxLayout()
-        # Add margins to the layout
-        self.main_layout.setContentsMargins(100, 150, 100, 150)
         self.main_layout.addWidget(self.title_label)
         self.main_layout.addLayout(self.button_layout)
         self.main_layout.addWidget(self.file_label)
-        self.main_layout.addStretch()
-
         self.setLayout(self.main_layout)
+
+
 
 
         # Set the stylesheet for the welcome page buttons
@@ -113,24 +107,83 @@ class WelcomePage(QWidget):
 
 
 class Page1(QWidget):
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Emissions Calculator")
-        self.setGeometry(100, 100, 800, 600)
-        self.back_button = QPushButton("Back to Menu", clicked=self.back_to_menu)
 
-        # Add the buttons to the layout
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.back_button)
-        self.setLayout(self.layout)
+        layout = QVBoxLayout()
+        grid = QGridLayout()
+        # Stretch the rows
+        grid.setRowStretch(0, 1)
 
+        main_label = QLabel("Select percentages of students travelling by each transport method", self)
+        layout.addWidget(main_label)
+        # Add stretch to the first row
+        grid.setRowStretch(0, 1)
 
-    
+        scotland_label = QLabel("Scotland", self)
+        grid.addWidget(scotland_label, 0, 0)
 
+        bus_scot_label = QLabel("Bus %", self)
+        grid.addWidget(bus_scot_label, 1, 0)
+        self.bus_scot = QComboBox(self)
+        for i in range(101):
+            self.bus_scot.addItem(str(i))
+        grid.addWidget(self.bus_scot, 1, 1)
 
-        # Style the buttons
+        car_scot_label = QLabel("Car %", self)
+        grid.addWidget(car_scot_label, 2, 0)
+        self.car_scot = QComboBox(self)
+        for i in range(101):
+            self.car_scot.addItem(str(i))
+        grid.addWidget(self.car_scot, 2, 1)
+
+        rail_scot_label = QLabel("Train %", self)
+        grid.addWidget(rail_scot_label, 3, 0)
+        self.rail_scot = QComboBox(self)
+        for i in range(101):
+            self.rail_scot.addItem(str(i))
+        grid.addWidget(self.rail_scot, 3, 1)
+
+        # UK
+        uk_label = QLabel("Rest of UK", self)
+        grid.addWidget(uk_label, 0, 2)
+
+        plane_uk_label = QLabel("Plane %", self)
+        grid.addWidget(plane_uk_label, 1, 2)
+        self.plane_uk = QComboBox(self)
+        for i in range(101):
+            self.plane_uk.addItem(str(i))
+        grid.addWidget(self.plane_uk, 1, 3)
+
+        car_uk_label = QLabel("Car %", self)
+        grid.addWidget(car_uk_label, 2, 2)
+        self.car_uk = QComboBox(self)
+        for i in range(101):
+            self.car_uk.addItem(str(i))
+        grid.addWidget(self.car_uk, 2, 3)
+
+        rail_uk_label = QLabel("Train %", self)
+        grid.addWidget(rail_uk_label, 3, 2)
+        self.rail_uk = QComboBox(self)
+        for i in range(101):
+            self.rail_uk.addItem(str(i))
+        grid.addWidget(self.rail_uk, 3, 3)
+
+        layout.addLayout(grid)
+        # add back button
+        back_button = QPushButton("Back to Menu", clicked=self.back_to_menu)
+        layout.addWidget(back_button)
+        self.setLayout(layout)
+        
+        
         self.setStyleSheet("""
+            QWidget {
+                border: 2px solid #2d3436;
+                border-radius: 5px;
+                padding: 2px;
+                background-color: #dfe6e9;
+            }
             QPushButton {
                 background-color: #0984e3;
                 color: #ffffff;
@@ -143,6 +196,20 @@ class Page1(QWidget):
             QPushButton:hover {
                 background-color: #74b9ff;
             }
+            QLabel {
+                font-size: 16px;
+                font-weight: bold;
+                border: none;
+            }
+            QComboBox {
+                background-color: #74b9ff;
+                color: #ffffff;
+                font-weight: bold;
+                height: 40px;
+                border: none;
+                border-radius: 5px;
+                margin: 5px 0;
+            }                                       
         """)
 
 
@@ -150,6 +217,10 @@ class Page1(QWidget):
     def back_to_menu(self):
         # Go back to menu but keep the selected file
         self.parent().setCentralWidget(WelcomePage())
+
+    def get(self):
+        text = self.input.text()
+        print(text)
 
 
 class Page2(QWidget):
